cc920d28586ce2c28d2e8eb5cbffc600def9dbe9
David Ward <david.a.ward@duke.edu>
2022-09-20 13:32:18 -0400

    Fixed all the PEP-8 errors that had accumulated so far this semester

--- blood_calculator.py
+++ blood_calculator.py
@@ -16,28 +16,34 @@ def interface():
             LDL_driver()
         elif choice == "3":
             total_driver()
-            
+
+
 def input_value(test_name):
     test_input = input("Enter the {} value:".format(test_name))
     return int(test_input)
-    
+
+
 def check_HDL(HDL_value):
     if HDL_value >= 60:
         return "Normal"
-    elif 40<= HDL_value <60:
+    elif 40 <= HDL_value < 60:
         return "Borderline Low"
     else:
         return "Low"
+
 
 def HDL_driver():
     test_name = "HDL"
     hdl_value = input_value(test_name)
     answer = check_HDL(hdl_value)
     output_result(test_name, hdl_value, answer)
-    
+
+
 def output_result(test_name, hdl_value, charac):
-    print("The results for a {} value of {} is {}".format(test_name, hdl_value, charac))
-    
+    print("The results for a {} value of {} is {}"
+          .format(test_name, hdl_value, charac))
+
+
 def check_LDL(LDL_value):
     if LDL_value < 130:
         return "Normal"
@@ -47,13 +53,15 @@ def check_LDL(LDL_value):
         return "High"
     else:
         return "Very high"
-        
+
+
 def LDL_driver():
     test_name = "LDL"
     ldl_value = input_value(test_name)
     answer = check_LDL(ldl_value)
     output_result(test_name, ldl_value, answer)
-    
+
+
 def check_total_cholesterol(total_value):
     if total_value < 200:
         return "Normal"
@@ -61,12 +69,14 @@ def check_total_cholesterol(total_value):
         return "Borderline high"
     else:
         return "High"
-        
+
+
 def total_driver():
     test_name = "Total Cholesterol"
     total_value = input_value(test_name)
     answer = check_total_cholesterol(total_value)
     output_result(test_name, total_value, answer)
+
 
 if __name__ == "__main__":
     interface()
--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,11 +1,11 @@
 import pytest
 
+
 @pytest.mark.parametrize("HDL_input, expected",
-    [(85, "Normal"),
-     (45, "Borderline Low"),
-     (20, "Low")])
+                         [(85, "Normal"),
+                          (45, "Borderline Low"),
+                          (20, "Low")])
 def test_check_HDL_Normal(HDL_input, expected):
     from blood_calculator import check_HDL
     answer = check_HDL(HDL_input)
     assert answer == expected
-    
