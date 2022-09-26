12e974bcc5a6db1b0bfc60e8a5998fbd9afa515a
David Ward <david.a.ward@duke.edu>
2022-09-26 12:48:46 -0400

    Converted database from a list of patients to a dictionary of patients with id as the key

--- database.py
+++ database.py
@@ -13,20 +13,18 @@ def create_patient_entry(patient_first_name,
 def print_database(db):
     for patient in db:
         print(patient)
-        print("Name: {}, id: {}, age: {}".format(get_full_name(patient),
-                                                 patient["Id"],
-                                                 patient["Age"]))
+        print("Name: {}, id: {}, age: {}".format(get_full_name(db[patient]),
+                                                 db[patient]["Id"],
+                                                 db[patient]["Age"]))
 
 def get_full_name(patient):
     full_name = "{} {}".format(patient["First Name"], patient["Last Name"])
     return full_name
 
 def find_patient(db, id_no):
-    for patient in db:
-        if patient["Id"] == id_no:
-            return patient
-    return False
-
+    patient = db[id_no]
+    return patient
+    
 
 def add_test_to_patient(db, id_no, test_name, test_value):
     patient = find_patient(db, id_no)
@@ -41,15 +39,15 @@ def adult_or_minor(patient):
 
 
 def main():
-    db = []
-    db.append(create_patient_entry("Ann", "Ables", 11, 30))
-    db.append(create_patient_entry("Bob", "Boyles", 22, 34))
-    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    db = {}
+    db[11] = create_patient_entry("Ann", "Ables", 11, 30)
+    db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
+    db[3] = create_patient_entry("Chris", "Chou", 3, 25)
     print_database(db)
     add_test_to_patient(db, 3, "HDL", 100)
     print_database(db)
-    print("Patient {} is a {}".format(get_full_name(db[2]),
-                                      adult_or_minor(db[2])))
+    #print("Patient {} is a {}".format(get_full_name(db[2]),
+    #                                  adult_or_minor(db[2])))
                                       
 
 
