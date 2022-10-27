0186c3db0b49f5e53aacaf762151cd7581b3a705
David Ward <david.a.ward@duke.edu>
2022-10-26 20:48:26 -0400

    Add a client case with an invalid request to "/get_results/<patient_id>" to demonstrate server validation

--- db_client.py
+++ db_client.py
@@ -17,3 +17,7 @@ if r.status_code == 200:
 else:
     print(r.text)
 
+# Invalid request to see what happens
+r = requests.get("http://127.0.0.1:5000/get_results/43")
+print(r.status_code)
+print(r.text)
